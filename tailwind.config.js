9e409f29940f56e5dc0df79c01efe9e201ddeb78
j-0-n-e-z <j000neeez@gmail.com>
2023-05-15 15:58:56 +0500

    refactor

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,12 +8,15 @@ module.exports = {
 			colors: {
 				softBlue: 'hsl(215, 51%, 70%)',
 				cyan: 'hsl(178, 100%, 50%)',
-				mainBg: 'hsl(217, 54%, 11%)',
-				cardBg: 'hsl(216, 50%, 16%)',
+				main: 'hsl(217, 54%, 11%)',
+				card: 'hsl(216, 50%, 16%)',
 				line: 'hsl(215, 32%, 27%)'
 			},
 			fontFamily: {
 				Outfit: ['Outfit', ...defaultTheme.fontFamily.sans]
+			},
+			fontSize: {
+				base: '15px'
 			}
 		}
 	},
