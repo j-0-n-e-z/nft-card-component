3a19cc5a49836c2d1bc90c22dca0d5fa6f6303ee
j-0-n-e-z <j000neeez@gmail.com>
2023-05-15 15:07:52 +0500

    refactor

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 module.exports = {
 	content: ['./dist/**/*.{html,js}'],
 	theme: {
@@ -9,6 +11,9 @@ module.exports = {
 				mainBg: 'hsl(217, 54%, 11%)',
 				cardBg: 'hsl(216, 50%, 16%)',
 				line: 'hsl(215, 32%, 27%)'
+			},
+			fontFamily: {
+				Outfit: ['Outfit', ...defaultTheme.fontFamily.sans]
 			}
 		}
 	},
